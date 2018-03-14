dd22e0cd8d3dc7b06d1f4d6663e9fcb3ef04f3bb
Popescu George Calin <george-calin.popescu@my.fmi.unibuc.ro>
2018-03-14 12:42:51 +0200

    RollABall-v1.0
    Ceva pe acolo

---
+++ rollABall/Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    // Use this for initialization
+    // Update is called once per frame
+    private Vector3 aux;
+    void Start()
+    {
+        aux = new Vector3(15, 30, 45);
+    }
+    void Update () {
+        transform.Rotate(aux * Time.deltaTime);
+		
+	}
+}
